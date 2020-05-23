9cbd50ec2e68e053c35b5cb1d5c2d2f0ac93a2b6
Abdullah Harun Siddiqui <31738460+ahsharun@users.noreply.github.com>
2020-05-24 01:05:46 +0530

    Interval List Intersections
    Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
    
    Return the intersection of these two interval lists.
    
    (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
    Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
    Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
    Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
     
    
    Note:
    
    0 <= A.length < 1000
    0 <= B.length < 1000
    0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
    NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.

---
+++ 23.interval-list-intersections.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        List<int[]> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while(i < A.length && j < B.length){
+            int maxStart = Math.max(A[i][0], B[j][0]);
+            int minEnd = Math.min(A[i][1], B[j][1]);
+            if(maxStart <= minEnd){
+                list.add(new int[]{maxStart, minEnd});
+            }
+            if(A[i][1] < B[j][1]){
+                i++;
+            }
+            else if(A[i][1] > B[j][1]){
+                j++;
+            }
+            else{
+                i++;
+                j++;
+            }
+        }
+        int[][] res = new int[list.size()][2];
+        i = 0;
+        for(int[] arr : list){
+            res[i++] = arr;
+        }
+
+        return res;
+    }
+}
